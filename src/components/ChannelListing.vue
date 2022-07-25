3f8a85fab0c9b318666afab1e05ae4ace8de3df3
Chris Roberts <chris@829llc.com>
2022-07-25 18:16:00 -0400

    Added channel logo component

--- src/components/ChannelListing.vue
+++ src/components/ChannelListing.vue
@@ -1,35 +1,17 @@
-<script setup></script>
+<script setup>
+import ChannelListingLogo from './ChannelListingLogo.vue';
+</script>
 
 <template>
 	<aside class="channel-listing">
-			<div class="channel-listing__logo">
-				<span class="channel-listing__logo-img" style="--logo-path: url(/images/channels/cw.svg);"></span>
-			</div>
-
-			<div class="channel-listing__logo">	
-				<span class="channel-listing__logo-img" style="--logo-path: url(/images/channels/amc.svg);"></span>
-			</div>
-
-			<div class="channel-listing__logo">
-				<span class="channel-listing__logo-img" style="--logo-path: url(/images/channels/cbs.svg);"></span>	
-			</div>
-
-			<div class="channel-listing__logo">
-				<span class="channel-listing__logo-img" style="--logo-path: url(/images/channels/fox.svg);"></span>	
-			</div>
-
-			<div class="channel-listing__logo">
-				<span class="channel-listing__logo-img" style="--logo-path: url(/images/channels/ifc.svg);"></span>	
-			</div>
-
-			<div class="channel-listing__logo">
-				<span class="channel-listing__logo-img" style="--logo-path: url(/images/channels/bet.svg);"></span>	
-			</div>
-
-			<div class="channel-listing__logo">
-				<span class="channel-listing__logo-img" style="--logo-path: url(/images/channels/amc.svg);"></span>	
-			</div>
-		</aside>
+		<ChannelListingLogo channel="amc" logo="amc.svg"></ChannelListingLogo>
+		<ChannelListingLogo channel="bet" logo="bet.svg"></ChannelListingLogo>
+		<ChannelListingLogo channel="cbs" logo="cbs.svg"></ChannelListingLogo>
+		<ChannelListingLogo channel="cw" logo="cw.svg"></ChannelListingLogo>
+		<ChannelListingLogo channel="fox" logo="fox.svg"></ChannelListingLogo>
+		<ChannelListingLogo channel="ifc" logo="ifc.svg"></ChannelListingLogo>
+		<ChannelListingLogo channel="espn" logo="espn.svg"></ChannelListingLogo>
+	</aside>
 </template>
 
 <style lang="scss" scoped>
@@ -46,27 +28,5 @@
 	transition: var(--transition-duration) all ease-in-out;
 	z-index: 10;
 	box-shadow: 10px 0 20px rgba(0, 0, 0, 0.75);
-
-	&__logo {
-		padding: var(--block-padding);
-		background: #333;
-		height: var(--block-height);
-		border-radius: var(--block-corner-radius);
-	}
-
-	&__logo-img {
-		-webkit-mask-image: var(--logo-path);
-		-webkit-mask-size: contain;
-		-webkit-mask-repeat: no-repeat;
-		-webkit-mask-position: center;
-		// mask-image: var(--logo-path);
-		// mask-size: contain;
-		// mask-repeat: no-repeat;
-		// mask-position: center;
-		background: #FFF;
-		display: block;
-		width: 100%;
-		height: 100%;
-	}
 }
 </style>
---
+++ src/components/ChannelListingLogo.vue
@@ -0,0 +1,48 @@
+<script setup>
+import {computed} from 'vue';
+
+const props = defineProps({
+	logo: String,
+	channel: String
+});
+
+const logoPath = computed(() => {
+	return new URL(`../assets/images/${props.logo}`, import.meta.url);
+});
+</script>
+
+<template>
+	<div class="channel-listing__logo">
+		<span class="channel-listing__logo-img"
+			v-bind:data-channel="channel"
+			:style="`--logo-path: url('${logoPath}')`"
+		>
+		</span>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.channel-listing {
+	&__logo {
+		padding: var(--block-padding);
+		background: #333;
+		height: var(--block-height);
+		border-radius: var(--block-corner-radius);
+	}
+
+	&__logo-img {
+		-webkit-mask-image: var(--logo-path);
+		-webkit-mask-size: contain;
+		-webkit-mask-repeat: no-repeat;
+		-webkit-mask-position: center;
+		mask-image: var(--logo-path);
+		mask-size: contain;
+		mask-repeat: no-repeat;
+		mask-position: center;
+		background: #FFF;
+		display: block;
+		width: 100%;
+		height: 100%;
+	}
+}
+</style>
