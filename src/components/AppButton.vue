6b50e37033f541ab95d5e6d9f74eaf03edff8e81
Chris Roberts <chris@829llc.com>
2022-08-22 15:32:48 -0400

    Button styling

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -15,24 +15,34 @@
 @import '../assets/css/vars';
 
 .app-button {
+	--top-gap: 64px;
+
+	@include mobile {
+		--top-gap: 32px;
+	}
+
 	display: inline-flex;
 	gap: 16px;
+	font-size: 14px;
 	justify-content: center;
 	align-items: center;
 	text-shadow: none;
-	font-weight: bold;
+	text-transform: uppercase;
+	letter-spacing: 0.5px;
 	color: #FFF;
-	background: orange;
-	padding: 16px 64px;
-	border-radius: 32px;
+	padding: 16px 32px;
 	text-decoration: none;
-	margin-top: 64px;
-	box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.75);
+	margin-top: var(--top-gap);
+	box-shadow: 0 0 8px rgba(0, 0, 0, 0.75);
 	transition: var(--transition-duration) all ease-in-out;
-	border: 3px solid rgba(255, 255, 255, 0.50);
+	border: 1px solid rgba(255, 255, 255, 0.50);
+	background: #ff7b0d; /* Old browsers */
+	background: -moz-linear-gradient(left,  #ff7b0d 0%, #ffa84c 100%); /* FF3.6-15 */
+	background: -webkit-linear-gradient(left,  #ff7b0d 0%,#ffa84c 100%); /* Chrome10-25,Safari5.1-6 */
+	background: linear-gradient(to right,  #ff7b0d 0%,#ffa84c 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
 
 	&:hover {
-		box-shadow: 2px 2px 16px 10px rgba(219, 140, 50, 0.66);
+		box-shadow: 0px 0 16px 0 rgba(219, 140, 50, 0.66);
 	}
 }
 </style>
