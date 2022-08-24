791ee6674064e4ce595ef62a5321308f477c25ff
Chris Roberts <chris@829llc.com>
2022-08-24 09:27:51 -0400

    Fix timeslot bug with shows without network names

--- src/state/timeslots.js
+++ src/state/timeslots.js
@@ -9,6 +9,7 @@ const timeSlots = computed(() => {
 
 	shows.value.forEach(show => {
 		if (!show?.airdate && !show?.airtime) {return}; //skip shows without the required values
+		if (!show?.show?.network?.name) {return}; //skip shows without a network name because they won't have a channel
 		if (show.airdate !== today.value) {return}; //skip shows that don't air today
 		const startDate = DateTime.fromISO(`${show.airdate}T${show.airtime}`);
 		
