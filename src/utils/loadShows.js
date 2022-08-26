088f2c8e3ca86587369f426df932ac6ff6cc49df
Chris Roberts <chris@829llc.com>
2022-08-26 14:57:09 -0400

    Updated currentID to find the first show that matches the first time slot

--- src/utils/loadShows.js
+++ src/utils/loadShows.js
@@ -2,6 +2,7 @@ import loading from '../state/loading';
 import shows from '../state/shows';
 import currentShowID from '../state/currentShowID';
 import channels from '../state/channels';
+import timeSlots from '../state/timeslots';
 
 const loadShows = async (url) => {
 	const response = await fetch(url);
@@ -11,8 +12,14 @@ const loadShows = async (url) => {
 	if (data) {
 		shows.value = data;
 		
-		const arr = Object.keys(channels.value).map(key => channels.value[key]);		
-		currentShowID.value = arr[0].shows[0].id;
+		const firstTimeSlot = timeSlots.value[0];
+
+		for (const channel in channels.value) {
+			if (channels.value[channel].shows.find(show => show.start === firstTimeSlot)) {
+				const firstShow = channels.value[channel].shows.find(show => show.start === firstTimeSlot);
+				currentShowID.value = firstShow.id;
+			}
+		}
 	}
 
 	loading.value = false;
